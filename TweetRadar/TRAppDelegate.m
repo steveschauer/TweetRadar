4b00331ee0752c9a99265deb5b2ed14a3c58baba
steveschauer <steve@steveschauer.org>
2014-10-03 09:30:43 -0700

    Update app delegate

--- TweetRadar/TRAppDelegate.m
+++ TweetRadar/TRAppDelegate.m
@@ -15,12 +15,12 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Add your Parse ID and key here:
-    [Parse setApplicationId:@"J7Egxihh0whFZafQUAMDJHXJ4bsK7ExheAzcjReP" clientKey:@"cQjfw1inSA9J5Fcblewexg1o0X9ffKQoXq5Mi3co"];
+    [Parse setApplicationId:@"<Your Parse Application ID>" clientKey:@"<Your Parse Client Key"];
     
     // Add your Twitter keys and secrets here:
-    [PFTwitterUtils initializeWithConsumerKey:@"w9UkgJefenCl0VjtR9yEa0qB1" consumerSecret:@"nW0MHfMLqFtfRr2RBoCMfjV0jbQ5HFdcDbDaVj1m6Q3zpfC6bn"];
-    [[PFTwitterUtils twitter] setAuthToken:@"46272995-RC3fHUatSsmi4sclRaxRCw7iVn0is1oHK6JIXe3QL"];
-    [[PFTwitterUtils twitter] setAuthTokenSecret:@"O0QU4cZadhPzt3JZLjTh0fUv0ypDfcQ292iRbVdaAc7xV"];
+    [PFTwitterUtils initializeWithConsumerKey:@"<Your Twitter Consumer Key>" consumerSecret:@"<Your Twitter Consumer Secret>"];
+    [[PFTwitterUtils twitter] setAuthToken:@"<Your Twitter Auth Token>"];
+    [[PFTwitterUtils twitter] setAuthTokenSecret:@"<Your Twitter Auth Token Secret>"];
     
     // kick off the tweet retrieval
     [TRStorage store];
